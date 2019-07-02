3e1db531fd0410318cbee32649b785edf71cc301
lizj2 <907257608@qq.com>
2019-07-02 09:14:43 +0800

    构造函数

---
+++ Chapter5/src/Demo51.java
@@ -0,0 +1,41 @@
+/**
+ * <pre>
+ * desc £ºTODO
+ * author £ºlizj
+ * date £º2019-07-02 08:19
+ * </pre>
+ */
+public class Demo51 {
+    public static void main(String[] args) {
+        Test51 test51 = new Test51();
+        System.out.println(test51.str);
+
+        Test52 test52 = new Test52();
+        System.out.println(test52.str);
+
+        Test53 test53 = new Test53("sssss");
+        System.out.println(test53.str);
+
+
+
+
+    }
+
+}
+
+
+class Test51{
+    String str;
+}
+
+class Test52{
+    String str = "abc";
+}
+
+class Test53{
+    String str = "abc";
+
+    public Test53(String str) {
+        this.str = str;
+    }
+}
