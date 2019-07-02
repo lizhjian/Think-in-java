5ab03f887e243fb1369e7d8ad326a01073efe4c0
lizj2 <907257608@qq.com>
2019-07-02 21:13:58 +0800

    验证this调用

---
+++ Chapter5/src/Demo58.java
@@ -0,0 +1,20 @@
+/**
+ * <pre>
+ * desc £ºTODO
+ * author £ºlizj
+ * date £º2019-07-02 21:11
+ * </pre>
+ */
+public class Demo58 {
+}
+
+class Dog5{
+    void eat(){
+        sleep();
+        this.sleep();
+
+    }
+    void sleep(){
+
+    }
+}
