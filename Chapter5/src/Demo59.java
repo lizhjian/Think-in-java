7d405d4e38cb7b6a8e38c0f0e6b22f4cf0799e6c
lizj2 <907257608@qq.com>
2019-07-03 08:13:43 +0800

    构造函数

---
+++ Chapter5/src/Demo59.java
@@ -0,0 +1,22 @@
+/**
+ * <pre>
+ * desc £ºTODO
+ * author £ºlizj
+ * date £º2019-07-02 21:32
+ * </pre>
+ */
+public class Demo59 {
+}
+
+
+class Dog9{
+    int i;
+
+    public Dog9() {
+        this(1);
+    }
+
+    public Dog9(int i) {
+        this.i = i;
+    }
+}
