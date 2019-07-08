6d5f87d4021458632e32d398eb94011f3e565bf3
lizj2 <907257608@qq.com>
2019-07-08 20:43:04 +0800

    final类无法被继承

---
+++ Chapter7/src/Demo722.java
@@ -0,0 +1,20 @@
+/**
+ * <pre>
+ * desc £ºTODO
+ * author £ºlizj
+ * date £º2019-07-08 20:41
+ * </pre>
+ */
+//public class Demo722 extends Test722{
+public class Demo722 {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+final class Test722{
+    void show(){
+        System.out.println("show");
+    }
+        }
