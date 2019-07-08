e3e53fb65d7de402b9b54ec8d542d7aa237d6de2
lizj2 <907257608@qq.com>
2019-07-08 20:40:34 +0800

    final方法

---
+++ Chapter7/src/Demo720.java
@@ -0,0 +1,23 @@
+/**
+ * <pre>
+ * desc £ºTODO
+ * author £ºlizj
+ * date £º2019-07-08 20:38
+ * </pre>
+ */
+public class Demo720 {
+
+
+
+    public static void main(String[] args) {
+        Test720 test720 = new Test720();
+        test720.f();
+    }
+}
+
+
+class Test720{
+    public final void f(){
+
+    }
+}
--- Chapter7/src/FinalArguments.java
+++ Chapter7/src/FinalArguments.java
@@ -6,14 +6,20 @@
  * </pre>
  */
 public class FinalArguments {
+    private String str ="abc";
     void with(final Gizmo g){
         //g = new Gizmo();
     }
 
     public static void main(String[] args) {
-
+        FinalArguments finalArguments = new FinalArguments();
+        System.out.println(finalArguments.str);
     }
 }
 class Gizmo{
-    public void spin(){}
+    public void spin(){
+        FinalArguments finalArguments = new FinalArguments();
+        //System.out.println(finalArguments.str);  私有方法除了本类就不好使
+
+    }
 }
