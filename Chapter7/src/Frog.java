25fdfa258f998c4aae8ebb564b42aa31513d9cf0
lizj2 <907257608@qq.com>
2019-07-08 19:56:36 +0800

    向上转型

---
+++ Chapter7/src/Frog.java
@@ -0,0 +1,39 @@
+/**
+ * <pre>
+ * desc £ºTODO
+ * author £ºlizj
+ * date £º2019-07-08 19:47
+ * </pre>
+ */
+public class Frog extends Amphibian{
+
+//    @Override
+//    void show() {
+//        //super.show();
+//        System.out.println("Frog");
+//    }
+
+    @Override
+    void say(Amphibian amphibian) {
+        //super.say(amphibian);
+        System.out.println("say");
+    }
+
+    public static void main(String[] args) {
+        Amphibian amphibian = new Amphibian();
+        amphibian.show();
+        amphibian.say(new Frog());
+
+
+    }
+}
+
+class Amphibian{
+    void show(){
+        System.out.println("Amphibian");
+    }
+
+    void say(Amphibian amphibian){
+        amphibian.show();
+    }
+}
