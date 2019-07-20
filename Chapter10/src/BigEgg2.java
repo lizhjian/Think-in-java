d0fafae1fad1e384f1ac93913704c637ff5cef25
lizj2 <907257608@qq.com>
2019-07-20 12:33:05 +0800

    覆盖内部类

---
+++ Chapter10/src/BigEgg2.java
@@ -0,0 +1,21 @@
+/**
+ * <pre>
+ * desc £ºÄÚ²¿Àà¸²¸Ç
+ * author £ºlizj
+ * date £º2019-07-20 10:47
+ * </pre>
+ */
+class Egg2{
+    protected class Yolk{
+        public Yolk() {
+            System.out.println("Egg2.Yolk()");
+        }
+        public void f(){
+            System.out.println("Egg2.Yolk.f()");
+        }
+    }
+}
+
+
+public class BigEgg2 {
+}
