2d36a00f80d667331c0f079a02dab000295bb0a3
lizj2 <907257608@qq.com>
2019-07-22 22:56:29 +0800

    LinkedHashSet

---
+++ Chapter11/src/Demo1119.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+/**
+ * <pre>
+ * desc £ºTODO
+ * author £ºlizj
+ * date £º2019-07-22 22:51
+ * </pre>
+ */
+public class Demo1119 {
+
+    public static void main(String[] args) {
+        Set<String> s = new HashSet<>();
+        s.add("aaa");
+        s.add("bbb");
+        s.add("ccc");
+        s.add("ddd");
+        System.out.println(s);
+
+        Set<String> set = new LinkedHashSet<>(s);
+        Set<String> set1 = new LinkedHashSet<>(s);
+
+        System.out.println(set);
+
+        Iterator<String> iterator = s.iterator();
+
+        while (iterator.hasNext()){
+            set.add(iterator.next());
+        }
+        System.out.println(set);
+        for (String str:s){
+            set1.add(str);
+        }
+        System.out.println(set1);
+    }
+}
